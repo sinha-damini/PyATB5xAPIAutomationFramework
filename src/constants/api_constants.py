67a04d3732199bc35b39fa0780f9e22cd6393059
sinha-damini <damini802301sinha@gmail.com>
2025-01-07 19:56:13 +0530

    07th Jan

---
+++ src/constants/api_constants.py
@@ -0,0 +1,14 @@
+# APIConstants - Class which contain all the endpoints.
+# Keep the URLs
+
+
+class APIConstants(object):
+    def base_url(self):
+        return "https://restful-booker.herokuapp.com"
+
+    def url_create_booking(self):
+        return "https://restful-booker.herokuapp.com/booking"
+
+    def url_create_token(self):
+        return "https://restful-booker.herokuapp.com/auth"
+
